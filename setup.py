be3a29ce3516dda664787ba9810c69f6cc574511
Sebastien Awwad <sebastien.awwad@gmail.com>
2016-04-15 15:35:06 -0400

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name = 'depresolve',
+    version = '0.4',
+    description = 'Package dependency scraping, dependency conflict detection, and dependency conflict resolution, for PyPI, integrating with pip.',
+    author = 'Sebastien Awwad',
+    author_email = 'sebastien.awwad@gmail.com',
+    url = 'https://github.com/awwad/depresolve',
+    install_requires = ['six']
+    keywords = 'update updater pypi dependency dependencies',
+    packages = ['depresolve']
+)
+
+
