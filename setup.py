9d00bbac01dd6072ea94054ab3fadb85d60500ed
Sebastien Awwad <sebastien.awwad@gmail.com>
2016-06-07 13:47:01 -0400

    correcting setup.py to have all packages

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name = 'depresolve',
-    version = '0.4',
+    version = '0.5',
     description = 'Package dependency scraping, dependency conflict detection, and dependency conflict resolution, for PyPI, integrating with pip.',
     author = 'Sebastien Awwad',
     author_email = 'sebastien.awwad@gmail.com',
     url = 'https://github.com/awwad/depresolve',
     install_requires = ['six'],
     keywords = 'update updater pypi dependency dependencies',
-    packages = ['depresolve']
+    packages = ['depresolve', 'depresolve.resolver', 'depresolve._external']
 )
 
 
